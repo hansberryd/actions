eca4ec6271076677b4bcd6bad7193e58f5f3c053
publishactions <publishactions@wheelhouse-tech.com.au>
2022-06-16 01:04:24 +0000

    Automatic publish from github.com/hansberryd/test-action

---
+++ invoke-binary.js
@@ -0,0 +1,19 @@
+const os = require("node:os");
+const child_process = require("child_process");
+
+function chooseBinary() {
+  let arch = os.arch();
+  let platform = os.platform();
+
+  // ...
+  if (platform === "linux" && arch === "x64") {
+    return `test-action-linux-amd64-d33f41c064aa19fc41546f66428993a7ff05e4f0`;
+  }
+  // ...
+}
+
+const binary = chooseBinary();
+const mainScript = `${__dirname}/${binary}`;
+const spawnSyncReturns = child_process.spawnSync(mainScript, {
+  stdio: "inherit",
+});
