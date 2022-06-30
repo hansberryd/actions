c4658989d44afe45443c7ca0f3a439574d1fbb1b
Automated Actions Publisher <actions_publisher@users.noreply.github.com>
2022-06-30 05:48:12 +0000

    Action packaged for distribution and usage.

--- hello-action/invoke-binary.js
+++ hello-action/invoke-binary.js
@@ -14,15 +14,15 @@ function chooseBinary() {
         "and identified architecture is: " +
         arch
     );
-    return `hello-action-linux-amd64-15f68b2ba97ec572ab72e967968b528b3b1fb673`;
+    return `hello-action-linux-amd64-502af2cdb5960bc04100fe4f6c330c519755bc8e`;
   } else if (platform === "linux" && arch === "386") {
-    return `hello-action-linux-386-15f68b2ba97ec572ab72e967968b528b3b1fb673`;
+    return `hello-action-linux-386-502af2cdb5960bc04100fe4f6c330c519755bc8e`;
   } else if (platform === "darwin" && arch === "x64") {
-    return `hello-action-darwin-amd64-15f68b2ba97ec572ab72e967968b528b3b1fb673`;
+    return `hello-action-darwin-amd64-502af2cdb5960bc04100fe4f6c330c519755bc8e`;
   } else if (platform === "windows" && arch === "x64") {
-    return `hello-action-windows-amd64-15f68b2ba97ec572ab72e967968b528b3b1fb673`;
+    return `hello-action-windows-amd64-502af2cdb5960bc04100fe4f6c330c519755bc8e`;
   } else if (platform === "windows" && arch === "386") {
-    return `hello-action-windows-386-15f68b2ba97ec572ab72e967968b528b3b1fb673`;
+    return `hello-action-windows-386-502af2cdb5960bc04100fe4f6c330c519755bc8e`;
   }
   // ...
 }
